c5355762e37615e336fead64b44233e1602138c2
Margarita Petkova <devnull@localhost>
2011-11-23 18:06:15 +0100

    removed extra directory

--- SLsimLib_cpp/include/galaxies.h
+++
@@ -1,9 +0,0 @@
-/*
- * galaxies.h
- *
- *  Created on: Mar 6, 2010
- *      Author: R.B. Metcalf
- */
-#include <Tree.h>
-
-void create_sersic(int n,double Ro,double f,double *center,double theta,double **x,long Nsources);
