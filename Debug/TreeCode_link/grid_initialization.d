5788bffa3a3e2442c261be85874f046488672c2a
Margarita Petkova <devnull@localhost>
2011-11-24 12:44:53 +0100

    removed Debug and Release folders

--- Debug/TreeCode_link/grid_initialization.d
+++
@@ -1,2 +0,0 @@
-TreeCode_link/grid_initialization.d TreeCode_link/grid_initialization.o:  \
- ../TreeCode_link/grid_initialization.c
--- Release/Kist/Kist.d
+++
@@ -1,7 +0,0 @@
-Kist/Kist.d Kist/Kist.o: ../Kist/Kist.c \
-  /Users/mpetkova/Code/SLsimLib/include/Kist.h \
-  /Users/mpetkova/Code/SLsimLib/include/point.h
-
-/Users/mpetkova/Code/SLsimLib/include/Kist.h:
-
-/Users/mpetkova/Code/SLsimLib/include/point.h:
