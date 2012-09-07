5c9f2e3c1106e0dead26d77460b2d0954cff1b55
Margarita Petkova <devnull@localhost>
2012-09-07 11:43:32 +0200

    Chnaged the star tree force calculation the AnaLens rayshooter to the recursive one, so that the rayshooting is now fully parallel too

--- FullRange/internal_rayshooter_nfw.cpp
+++ FullRange/internal_rayshooter_nfw.cpp
@@ -18,17 +18,6 @@
  */
 void AnaLens::rayshooterInternal(unsigned long Npoints, Point *i_points, bool kappa_off){
 	/* i_points need to be already linked to s_points */
-	double x_rescale[2];
-	static double convert_factor=0;
-    long i,j;
-    float dt = 0,tmp;
-
-    struct temp_data{
-    	double alpha[2];
-    	float gamma[3];
-    } *temp;
-
-    temp = (struct temp_data *) malloc(Npoints * sizeof(struct temp_data));
 
     if(this == NULL || !set){
     	ERROR_MESSAGE();
@@ -36,14 +25,24 @@ void AnaLens::rayshooterInternal(unsigned long Npoints, Point *i_points, bool ka
     	exit(0);
     }
 
-    convert_factor = star_massscale / Sigma_crit;
+	double convert_factor = star_massscale / Sigma_crit;
+	int i;
 
 #ifdef _OPENMP
-#pragma omp parallel for private(x_rescale, dt, i)
+#pragma omp parallel for
 #endif
     for(i = 0; i < Npoints; i++){
-    	i_points[i].dt = 0;
-    	i_points[i].gamma[2] = 0;
+    	double x_rescale[2];
+        long i,j;
+        float dt = 0,tmp = 0;
+    	double alpha[2];
+    	float gamma[3];
+
+    	alpha[0]=alpha[1]=0.0;
+    	gamma[0]=gamma[1]=gamma[2]=0.0;
+
+    	i_points[i].dt = 0.0;
+    	i_points[i].gamma[2] = 0.0;
 
     	// host lens
     	if(host_ro > 0){
@@ -82,33 +81,36 @@ void AnaLens::rayshooterInternal(unsigned long Npoints, Point *i_points, bool ka
     	// perturbations of host lens
     	if(perturb_Nmodes > 0){
     		i_points[i].kappa += lens_expand(perturb_beta,perturb_modes
-    				,perturb_Nmodes,i_points[i].x,temp[i].alpha,temp[i].gamma,&dt);
+    				,perturb_Nmodes,i_points[i].x,alpha,gamma,&dt);
 
-    		i_points[i].image->x[0] += temp[i].alpha[0];
-    		i_points[i].image->x[1] += temp[i].alpha[1];
+    		i_points[i].image->x[0] += alpha[0];
+    		i_points[i].image->x[1] += alpha[1];
 
     		if(!kappa_off){
-    			i_points[i].gamma[0] += temp[i].gamma[0];
-    			i_points[i].gamma[1] += temp[i].gamma[1];
+    			i_points[i].gamma[0] += gamma[0];
+    			i_points[i].gamma[1] += gamma[1];
     			i_points[i].dt += dt;
     		}
     		else
     			i_points[i].kappa = 0;
     	} // end of perturb modes
 
+    	alpha[0]=alpha[1]=0.0;
+    	gamma[0]=gamma[1]=gamma[2]=0.0;
+
     	// add substructure
     	if(substruct_implanted){
     		for(j=0;j<sub_N;++j){
-    			sub_alpha_func(temp[i].alpha,i_points[i].x,sub_Rcut[j],sub_mass[j],sub_beta,sub_x[j],Sigma_crit);
+    			sub_alpha_func(alpha,i_points[i].x,sub_Rcut[j],sub_mass[j],sub_beta,sub_x[j],Sigma_crit);
 
-    			i_points[i].image->x[0] += temp[i].alpha[0];
-    			i_points[i].image->x[1] += temp[i].alpha[1];
+    			i_points[i].image->x[0] += alpha[0];
+    			i_points[i].image->x[1] += alpha[1];
 
     			if(!kappa_off){
     				i_points[i].kappa += sub_kappa_func(i_points[i].x,sub_Rcut[j],sub_mass[j],sub_beta,sub_x[j],Sigma_crit);
-    				sub_gamma_func(temp[i].gamma,i_points[i].x,sub_Rcut[j],sub_mass[j],sub_beta,sub_x[j],Sigma_crit);
-    				i_points[i].gamma[0] += temp[i].gamma[0];
-    				i_points[i].gamma[1] += temp[i].gamma[1];
+    				sub_gamma_func(gamma,i_points[i].x,sub_Rcut[j],sub_mass[j],sub_beta,sub_x[j],Sigma_crit);
+    				i_points[i].gamma[0] += gamma[0];
+    				i_points[i].gamma[1] += gamma[1];
     				i_points[i].dt += sub_phi_func(i_points[i].x,sub_Rcut[j],sub_mass[j],sub_beta,sub_x[j],Sigma_crit);
 
     			}
@@ -122,58 +124,45 @@ void AnaLens::rayshooterInternal(unsigned long Npoints, Point *i_points, bool ka
     		i_points[i].dt *= to;
     	}
 
-    	// final operations to get the image positions and the inverse magnification
     	i_points[i].image->x[0] = i_points[i].x[0] - i_points[i].image->x[0];
     	i_points[i].image->x[1] = i_points[i].x[1] - i_points[i].image->x[1];
 
-    	i_points[i].invmag = (1-i_points[i].kappa)*(1-i_points[i].kappa)
-  	    				- i_points[i].gamma[0]*i_points[i].gamma[0] - i_points[i].gamma[1]*i_points[i].gamma[1];
-    } // end of first points loop
+    	alpha[0]=alpha[1]=0.0;
+    	gamma[0]=gamma[1]=gamma[2]=0.0;
 
-
-    if(stars_N > 0 && stars_implanted){
-    	// add stars for microlensing
-    	for(i = 0; i< Npoints; i++){
+    	if(stars_N > 0 && stars_implanted){
+    		// add stars for microlensing
     		substract_stars_disks(i_points[i].x,i_points[i].image->x,
     				&(i_points[i].kappa),i_points[i].gamma);
 
     		// do stars with tree code
-    		star_tree->force2D(i_points[i].x,temp[i].alpha,&tmp,temp[i].gamma,true);
+    		//star_tree->force2D(i_points[i].x,temp[i].alpha,&tmp,temp[i].gamma,true);
+    		star_tree->force2D_recur(i_points[i].x,alpha,&tmp,gamma,true);
 
-    		i_points[i].image->x[0] += convert_factor*temp[i].alpha[0];
-    		i_points[i].image->x[1] += convert_factor*temp[i].alpha[1];
+    		i_points[i].image->x[0] += convert_factor*alpha[0];
+    		i_points[i].image->x[1] += convert_factor*alpha[1];
 
     		if(!kappa_off){
     			i_points[i].kappa += convert_factor*tmp;
-    			i_points[i].gamma[0] += convert_factor*temp[i].gamma[0];
-    			i_points[i].gamma[1] += convert_factor*temp[i].gamma[1];
+    			i_points[i].gamma[0] += convert_factor*gamma[0];
+    			i_points[i].gamma[1] += convert_factor*gamma[1];
     		}
 
-    		// final operations to get the inverse magnification
-        	i_points[i].invmag = (1-i_points[i].kappa)*(1-i_points[i].kappa)
-      	    				- i_points[i].gamma[0]*i_points[i].gamma[0] - i_points[i].gamma[1]*i_points[i].gamma[1];
-    	}
+    	} // end of stars
 
-    } // end of stars
 
-#ifdef _OPENMP
-#pragma omp parallel for private(i)
-#endif
-      for(i = 0; i < Npoints; i++){
-    	  i_points[i].image->invmag=i_points[i].invmag;
-    	  i_points[i].image->kappa=i_points[i].kappa;
-    	  i_points[i].image->gamma[0]=i_points[i].gamma[0];
-    	  i_points[i].image->gamma[1]=i_points[i].gamma[1];
-    	  i_points[i].image->dt = i_points[i].dt;
-    	/*if(i<10){
-    		  cout << i_points[i].x[0] << " " << i_points[i].x[1] << " " << i_points[i].image->x[0] << " " << i_points[i].image->x[1] << " ";
-    		  cout << i_points[i].invmag << " " << i_points[i].kappa << " " << i_points[i].gamma[0] << " " << i_points[i].gamma[1] << " " << i_points[i].gamma[2] << endl;
-    	  }*/
-      }
+		// final operations to get the inverse magnification
+		i_points[i].invmag = (1-i_points[i].kappa)*(1-i_points[i].kappa)
+				- i_points[i].gamma[0]*i_points[i].gamma[0] - i_points[i].gamma[1]*i_points[i].gamma[1];
 
-      free(temp);
+    	i_points[i].image->invmag=i_points[i].invmag;
+    	i_points[i].image->kappa=i_points[i].kappa;
+    	i_points[i].image->gamma[0]=i_points[i].gamma[0];
+    	i_points[i].image->gamma[1]=i_points[i].gamma[1];
+    	i_points[i].image->dt = i_points[i].dt;
+    }
 
-      return ;
+    return ;
 }
 
 
@@ -187,7 +176,7 @@ void AnaLens::rayshooterInternal(double *ray, double *alpha, float *gamma, float
      double x_rescale[2];
      long j;
      double alpha_tmp[2];
-     float gamma_tmp[3], dt = 0,tmp;
+     float gamma_tmp[3], dt = 0,tmp = 0;
 
      gamma_tmp[0] = gamma_tmp[1] = gamma_tmp[2] = 0.0;
      alpha_tmp[0] = alpha_tmp[1] = 0.0;
@@ -256,7 +245,8 @@ void AnaLens::rayshooterInternal(double *ray, double *alpha, float *gamma, float
     	 substract_stars_disks(ray,alpha_tmp,kappa,gamma_tmp);
 
     	 // do stars with tree code
-    	 star_tree->force2D(ray,&alpha_tmp[0],&tmp,&gamma_tmp[0],true);
+    	 //star_tree->force2D(ray,alpha_tmp,&tmp,gamma_tmp,true);
+    	 star_tree->force2D_recur(ray,alpha_tmp,&tmp,gamma_tmp,true);
 
     	 alpha[0] += convert_factor*alpha_tmp[0];
     	 alpha[1] += convert_factor*alpha_tmp[1];
