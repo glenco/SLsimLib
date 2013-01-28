298b99fe49d2709029b6aca29cccc03424bd4ce9
Margarita Petkova <devnull@localhost>
2013-01-28 17:35:24 +0100

    moved test rayshooter functions to a separate file in order to reduce confusion

--- MultiPlane/multiplane.cpp
+++ MultiPlane/multiplane.cpp
@@ -1034,6 +1034,7 @@ void MultiLens::calc_error_test_multi(
 	for(j=1; j<Nhalos+1; j++)
 		dDl_halos[j] = Dl_halos[j] - Dl_halos[j-1];
 
+	rayshooterInternal(Npoints,i_points,kappa_off);
 	rayshooterInternal_halos_diff(Npoints,i_points,kappa_off,Dl_halos,dDl_halos);
 
 	delete[] Dl_halos;
