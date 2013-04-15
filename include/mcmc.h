b7b7f85496e290fbca819145aa5645c9af3fb100
Nicolas Tessore <nicolas.tessore@unibo.it>
2013-04-15 18:43:28 +0200

    The MCMC class now controls the full run of the MCMC algorithm.

--- include/mcmc.h
+++ include/mcmc.h
@@ -1,6 +1,12 @@
 #ifndef MCMC_H_
 #define MCMC_H_
 
+#include <monaco/likelihood.hpp>
+#include <monaco/metropolis.hpp>
+#include <cstddef>
+
+// macros to define the spec struct
+
 #define MCMC_SOURCE(s) typedef s source_type
 #define MCMC_LENS(l) typedef l lens_type
 
@@ -16,47 +22,103 @@ template<typename Spec>
 class MCMC
 {
 public:
-	typedef typename Spec::source_type source_type;
 	typedef typename Spec::lens_type lens_type;
+	typedef typename Spec::source_type source_type;
 	
-	typedef typename Spec::source_parameters source_parameters;
 	typedef typename Spec::lens_parameters lens_parameters;
+	typedef typename Spec::source_parameters source_parameters;
 	
-	typedef typename Spec::source_generator source_generator;
 	typedef typename Spec::lens_generator lens_generator;
+	typedef typename Spec::source_generator source_generator;
 	
-	class generator
-	{
-	public:
-		generator(source_generator& s, lens_generator& l)
-		: sgen(s), lgen(l)
-		{
-		}
-		
-		template<typename Parameters>
-		inline Parameters operator()(Parameters p)
-		{
-			p.source = sgen(p.source);
-			p.lens = lgen(p.lens);
-			return p;
-		}
-		
-	private:
-		source_generator& sgen;
-		lens_generator& lgen;
-	};
+	typedef typename Spec::likelihood likelihood;
 	
 	class parameters
 	{
 	public:
-		source_parameters source;
 		lens_parameters lens;
+		source_parameters source;
 		
-		parameters(source_parameters& s, lens_parameters& l)
-		: source(s), lens(l)
+		parameters()
+		{
+		}
+		
+		parameters(const lens_type& l, const source_type& s)
+		{
+			lens.from_lens(l);
+			source.from_source(s);
+		}
+		
+		parameters(const lens_parameters& l, const source_parameters& s)
+		: lens(l), source(s)
 		{
 		}
 	};
+	
+public:
+	MCMC(Model<lens_type, source_type>& model, lens_generator& lens_gen, source_generator& source_gen, likelihood& lh)
+	: model(model), gen(lens_gen, source_gen), lh(model, lh)
+	{
+	}
+	
+	template<typename Iterator>
+	void run(Iterator outp, const std::size_t n)
+	{
+		// load current parameters into chain
+		*outp = parameters(*model.lens, *model.source);
+		
+		// run metropolis algorithm
+		monaco::metropolis(gen, lh, outp, n);
+	}
+	
+private: /* classes */
+	class mcmc_generator
+	{
+	public:
+		mcmc_generator(lens_generator& l, source_generator& s)
+		: lgen(l), sgen(s)
+		{
+		}
+		
+		inline parameters operator()(parameters p)
+		{
+			p.lens = lgen(p.lens);
+			p.source = sgen(p.source);
+			return p;
+		}
+		
+	private:
+		lens_generator& lgen;
+		source_generator& sgen;
+	};
+	
+	class mcmc_likelihood
+	{
+	public:
+		static const bool is_logarithmic = monaco::likelihood_traits<likelihood>::logarithmic;
+		
+		mcmc_likelihood(Model<lens_type, source_type>& model, likelihood& lh)
+		: model(model), lh(lh)
+		{
+		}
+		
+		inline double operator()(const parameters& p)
+		{
+			p.lens.to_lens(*model.lens);
+			p.source.to_source(*model.source);
+			
+			return lh(model);
+		}
+		
+	private:
+		Model<lens_type, source_type>& model;
+		likelihood& lh;
+	};
+	
+private: /* members */
+	Model<lens_type, source_type> model;
+	mcmc_generator gen;
+	mcmc_likelihood lh;
 };
 
 #endif
