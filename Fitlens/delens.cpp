060c5e6039bffcc2b539a961cddeb92dc92e5d7b
Nicolas Tessore <nicolas.tessore@unibo.it>
2014-01-12 16:38:04 +0100

    Delens using Monte Carlo integration.

--- Fitlens/delens.cpp
+++ Fitlens/delens.cpp
@@ -8,16 +8,25 @@
 
 #include "../include/Tree.h"
 
-PixelMap delens(Lens& lens, const PixelMap& lensed, std::size_t Nrays, double res_factor)
+PixelMap delens(Lens& lens, const PixelMap& lensed, std::size_t Nrays, double res_factor, double mag_min)
 {
 	// number of points to shoot per batch, to not overflow memory
 	constexpr std::size_t Nbatch = 4096;
 	
-	// the resulting image should match the original
+	// the resulting image should match the original, with optional resolution change
 	PixelMap delensed = ((res_factor == 1) ? PixelMap(lensed) : PixelMap(lensed, res_factor));
 	
-	// clear the image
+	// clear the delensed image
 	delensed.Clean();
+	
+	// inverse pixel area of lensed image
+	double a_l = lensed.getResolution()*lensed.getResolution();
+	
+	// pixel area of delensed image
+	double a_d = delensed.getResolution()*delensed.getResolution();
+	
+	// maximum inverse magnification
+	double inv_mag_max = (mag_min == 0) ? 0 : std::abs(1./mag_min);
 	
 	// random number generator
 	std::mt19937 rng((std::random_device())());
@@ -25,11 +34,17 @@ PixelMap delens(Lens& lens, const PixelMap& lensed, std::size_t Nrays, double re
 	// uniform random number generator for positioning within a pixel
 	std::uniform_real_distribution<double> unif(-0.5*lensed.getResolution(), 0.5*lensed.getResolution());
 	
-	// inverse magnifications for each pixel
+	// inverse magnifications for each lensed pixel
 	std::vector<double> inv_mag(lensed.size());
 	
 	// calculate magnification for each pixel
 	{
+		// points at center each pixel
+		Point* points = NewPointArray(Nbatch, true);
+		
+		// points in source plane
+		Point* spoints = LinkToSourcePoints(points, Nbatch);
+		
 		// number of pixels
 		std::size_t Npixels = lensed.size();
 		
@@ -42,34 +57,37 @@ PixelMap delens(Lens& lens, const PixelMap& lensed, std::size_t Nrays, double re
 			// number of points in batch
 			std::size_t Npoints = std::min(Nbatch, Npixels);
 			
-			// points of each pixel
-			Point* points = NewPointArray(Npoints, true);
-			
 			// set the positions of each pixel
 			for(std::size_t j = 0; j < Npoints; ++j)
 				Utilities::PositionFromIndex(i+j, points[j].x, lensed.getNpixels(), lensed.getRange(), lensed.getCenter());
 			
-			// points in source plane
-			Point* spoints = LinkToSourcePoints(points, Npoints);
-			
 			// shoot rays to get magnifications
 			lens.rayshooterInternal(Npoints, points, false);
 			
-			// store magnification and sign for each pixel
+			// store absolute magnification for each pixel
 			for(std::size_t j = 0; j < Npoints; ++j, ++i)
 				inv_mag[i] = std::abs(points[j].invmag);
-			
-			// free arrays
-			FreePointArray(points);
-			FreePointArray(spoints);
 			
 			// decrese number of pixels
 			Npixels -= Npoints;
 		}
+		
+		// free arrays
+		FreePointArray(points);
+		FreePointArray(spoints);
 	}
 	
 	// delens image
 	{
+		// normalization for Monte Carlo integrals
+		std::vector<double> norm(delensed.size(), 0);
+		
+		// create an array of points for each ray
+		Point* points = NewPointArray(Nbatch, true);
+		
+		// create source points
+		Point* spoints = LinkToSourcePoints(points, Nbatch);
+		
 		// current pixel index
 		std::size_t i = 0;
 		
@@ -79,27 +97,24 @@ PixelMap delens(Lens& lens, const PixelMap& lensed, std::size_t Nrays, double re
 		// run in batches
 		while(true)
 		{
-			// create an array of points for each ray
-			Point* points = NewPointArray(Nbatch, true);
-			
 			// current point index
 			std::size_t Npoints = 0;
 			
 			// set positions for rays of each pixel
 			for(; i < lensed.size(); ++i)
 			{
-				// center of the pixel
-				double pos[2];
-				Utilities::PositionFromIndex(i, pos, lensed.getNpixels(), lensed.getRange(), lensed.getCenter());
-				
 				// number of rays in the pixel
-				std::size_t Npix = std::ceil(inv_mag[i]*Nrays);
+				std::size_t Npix = std::max(1., std::ceil(inv_mag[i]*Nrays)) + 0.5;
 				
-				// create random ray positions within pixel
+				// rays for pixel
 				for(; j < Npix && Npoints < Nbatch; ++j, ++Npoints)
 				{
-					points[Npoints].x[0] = pos[0] + unif(rng);
-					points[Npoints].x[1] = pos[1] + unif(rng);
+					// center of the pixel
+					Utilities::PositionFromIndex(i, points[Npoints].x, lensed.getNpixels(), lensed.getRange(), lensed.getCenter());
+					
+					// randomize position within pixel
+					points[Npoints].x[0] += unif(rng);
+					points[Npoints].x[1] += unif(rng);
 				}
 				
 				// check if batch is full
@@ -111,38 +126,52 @@ PixelMap delens(Lens& lens, const PixelMap& lensed, std::size_t Nrays, double re
 					j = 0;
 			}
 			
-			// create source points
-			Point* spoints = LinkToSourcePoints(points, Npoints);
-			
 			// shoot rays
 			lens.rayshooterInternal(Npoints, points, false);
 			
-			// create an image of the delensed rays
+			// add the delensed rays to output image
 			for(std::size_t k = 0; k < Npoints; ++k)
 			{
+				// check maximum magnitude
+				if(inv_mag_max && std::abs(points[k].invmag) > inv_mag_max)
+					continue;
+				
 				// get index for lensed pixel
 				long l = Utilities::IndexFromPosition(points[k].x, lensed.getNpixels(), lensed.getRange(), lensed.getCenter());
 				
 				// get index for delensed pixel
 				long d = Utilities::IndexFromPosition(spoints[k].x, delensed.getNpixels(), delensed.getRange(), delensed.getCenter());
 				
-				// number of rays for pixel
-				std::size_t Npix = std::ceil(inv_mag[l]*Nrays);
+				// check if the delensed pixel is in the image
+				if(d == -1)
+					continue;
 				
-				// if the pixel is in the image, add one count
-				if(d != -1)
-					delensed.AddValue(d, std::abs(points[k].invmag)*lensed[l]/Npix);
+				// number of rays in the pixel
+				std::size_t Npix = std::max(1., std::ceil(inv_mag[l]*Nrays)) + 0.5;
+				
+				// importance weight
+				double w = std::abs(points[k].invmag)/Npix*a_l;
+				
+				// add to sum
+				delensed.AddValue(d, w*lensed[l]/a_l);
+				
+				// add to normalization
+				norm[d] += w;
 			}
-			
-			// delete point arrays
-			FreePointArray(points);
-			if(spoints)
-				FreePointArray(spoints);
 			
 			// stop if all pixels and rays are done
 			if(i == lensed.size() && j == 0)
 				break;
 		}
+		
+		// delete point arrays
+		FreePointArray(points);
+		FreePointArray(spoints);
+		
+		// normalize the Monte Carlo integrals
+		for(std::size_t i = 0; i < delensed.size(); ++i)
+			if(norm[i] > 0)
+				delensed.AssignValue(i, a_d*delensed[i]/norm[i]);
 	}
 	
 	return delensed;
--- include/delens.h
+++ include/delens.h
@@ -10,29 +10,18 @@
  * This function shoots rays from the image plane to the source plane and uses
  * the given lensed PixelMap to recreate the PixelMap in the source plane.
  * 
- * This is done by distributing a number of rays uniformly over the pixel area
- * and raytracing them from the image plane to the source plane. The j'th ray
- * contributes a fraction of luminosity y_ij to the i'th pixel, where the
- * luminosity x_i in the image plane is
- * 
- * \sum_j |\mu_ij| y_ij = x_i
- * 
- * and \mu_ij is the magnification at the image plane position of the ray.
- * 
- * From this, the luminosity in the source plane can be reconstructed as
- * 
- * y_ij = |\mu_ij|^{-1} x_i/n
- * 
- * where n is the number of rays shot for the pixel.
- * 
  * The base number of rays per pixel in the image plane is given by the Nrays
  * argument. The actual number of rays shot per pixel is the base number
  * multiplied by the inverse magnification at the center of the pixel. This
- * will lead to a more uniform distribution of rays in the source plane.
+ * will lead to an approximately uniform distribution of rays in the source
+ * plane.
  * 
  * A higher-resolution version of the delensed image can be produced by setting
  * the res_factor argument to a value less than unity.
+ * 
+ * The minimum magnification that is considered can be specified. This prevents
+ * extremely demagnified areas of the image from degrading the reconstruction.
  */
-PixelMap delens(Lens& lens, const PixelMap& lensed, std::size_t Nrays = 10, double res_factor = 1);
+PixelMap delens(Lens& lens, const PixelMap& lensed, std::size_t Nrays = 10, double res_factor = 1, double mag_min = 1);
 
 #endif
