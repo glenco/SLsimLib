00e919920cf7006ba60c29234d7541f16d68455b
Fabio Bellagamba <fabio.bellagamba2@unibo.it>
2014-05-29 18:24:47 +0200

    Corrected bug in SourceShapelets::Hermite

--- AnalyticNSIE/source.cpp
+++ AnalyticNSIE/source.cpp
@@ -637,7 +637,7 @@ std::vector<double> SourceShapelets::Hermite(int n, PosType x)
 {
 	vector<double> hg(n);
 	hg[0] = 1.;
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i < n; i++)
 	{
 		if (i==1)
 			hg[1] = 2.*x;
