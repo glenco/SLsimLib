ae2618ebd12841e88a407d5de538da2d7bbfca8a
RB Metcalf <bmetcalf@gmail.com>
2014-06-16 08:39:16 +0200

    Changed to Clenshaw order 3.

--- TreeCode_link/isop_render.cpp
+++ TreeCode_link/isop_render.cpp
@@ -5,7 +5,8 @@
 	double ISOP::isop_render(Source& source, const double nodx[], const double nody[], double a0, double b0, double a1, double b1)
 	{
 		// TODO: figure out best integration rule to use
-		typedef Rules::ClenshawCurtis<5> rule;
+		typedef Rules::ClenshawCurtis<3> rule;
+		//typedef Rules::ClenshawCurtis<5> rule;
 		
 		// the precision and accuracy goals
 		double pg = 10e-2;
@@ -15,6 +16,6 @@
 		quadrature_result result = quadrature<rule>(ISOP::make_isop_map(SourceFunctor(source), nodx, nody), a0, b0, a1, b1, pg, ag);
 		
 		// return the result, error should be within the required limits
-		return result.value;///4;
+		return result.value;
 	}
 
