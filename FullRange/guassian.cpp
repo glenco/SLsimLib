b1df3c528d685d0fdbb43a5bf921f70f5e774099
RB Metcalf <bmetcalf@gmail.com>
2018-04-17 19:11:23 +0200

    Adding Gaussian random filed generator.

---
+++ FullRange/guassian.cpp
@@ -0,0 +1,8 @@
+//
+//  guassian.cpp
+//  NR
+//
+//  Created by Ben Metcalf on 17/04/2018.
+//
+
+#include "guassian.hpp"
---
+++ include/gaussian.h
@@ -0,0 +1,13 @@
+//
+//  guassian.hpp
+//  NR
+//
+//  Created by Ben Metcalf on 17/04/2018.
+//
+
+#ifndef guassian_hpp
+#define guassian_hpp
+
+#include <stdio.h>
+
+#endif /* guassian_hpp */
