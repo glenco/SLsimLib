e7ee4626a5af1c973a7fa1317b2de012975b7b5c
RB Metcalf <bmetcalf@gmail.com>
2019-06-27 11:11:18 +0200

    Small bug fix.

--- TreeCode_link/TreeDriver.cpp
+++ TreeCode_link/TreeDriver.cpp
@@ -1479,7 +1479,7 @@ RAY ImageInfo::closestRay(const Point_2d &y){
   
   Point *p;
   
-  double r2min = HUGE;
+  double r2min = HUGE_VALL;
   for(Kist<Point>::iterator it = imagekist->begin()
       ; it != imagekist->end()
       ; ++it){
