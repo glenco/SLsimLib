8c6dc2ed468b4233e1c183447e0c452dbce55028
RB Metcalf <bmetcalf@gmail.com>
2020-04-07 14:57:59 +0200

    Test needs to be removed.

--- MultiPlane/multimap.cpp
+++ MultiPlane/multimap.cpp
@@ -623,7 +623,7 @@ void LensHaloMultiMap::force_halo(double *alpha
       gamma[0] += t_gamma[0];
       gamma[1] += t_gamma[1];
       *kappa += t_kappa;
-    
+      *kappa = 1.0; // !!!
     }
   }
   return;
